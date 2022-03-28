8f8239d684f124c0cfa624f646cecd7e3c488527
D1SS007 <101556566+D1SS007@users.noreply.github.com>
2022-03-28 15:49:03 +0300

    Update Program.cs

--- ConsoleApp31/Program.cs
+++ ConsoleApp31/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp31
             Console.WriteLine("Введите количество выводов на экран");
             int amountOfOutputs = Convert.ToInt32(Console.ReadLine());
 
-            while (amountOfOutputs-- > 0)
+            for (int i = 0; i < amountOfOutputs; i++)
             {
                 Console.WriteLine(outputMessage);
             }
