87e334189028dacb03eec02510cf5e333be3af17
Alex Zverev <lordzver007@yandex.ru>
2022-03-28 15:40:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добрый день! Какое сообщение вы хотите вывести на экран?");
+            string outputMessage = Console.ReadLine();
+            Console.WriteLine("Введите количество выводов на экран");
+            int amountOfOutputs = Convert.ToInt32(Console.ReadLine());
+
+            while (amountOfOutputs-- > 0)
+            {
+                Console.WriteLine(outputMessage);
+            }
+        }
+    }
+}
